fff7818081898cfe31802dc6fa32b3680b2cd352
LSharples <user@Users-MacBook-Pro.local>
2020-08-18 16:42:17 +0100

    Hosting for demo

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -8,10 +8,13 @@ const awsmobile = {
     "aws_user_pools_id": "eu-west-2_zuXp6xWTL",
     "aws_user_pools_web_client_id": "2kd0vl8jbjjn9ahuaidj0b6nhu",
     "oauth": {},
+    "aws_content_delivery_bucket": "account.managedbills.com-dev",
+    "aws_content_delivery_bucket_region": "eu-west-2",
+    "aws_content_delivery_url": "http://account.managedbills.com-dev.s3-website.eu-west-2.amazonaws.com",
     "aws_appsync_graphqlEndpoint": "https://qa3cou2ygnbtfgcwewn4jgiezy.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_appsync_apiKey": "null",
+    "aws_appsync_apiKey": "null"
 };
 
 
